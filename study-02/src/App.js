ee35e2f18f131aa7d2c919c70db52fd98dba21a5
hu <604652763@qq.com>
2020-03-19 00:43:31 +0800

    '总结'

--- study-02/src/App.js
+++ study-02/src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+class ButtonGroup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { clickedList: props.buttons.map(x => false) }
+  }
+  onclickbtn = (index) => {
+    const clickedList = this.state.clickedList;
+    clickedList[index]=!clickedList[index]
+    this.setState({ clickedList: clickedList })
+  }
+  render() {
+    const buttonList = this.props.buttons.map(
+      (btn, index) =>
+        <button
+          onClick={() => this.onclickbtn(index)}
+          key={btn} id={btn}>
+          {this.state.clickedList[index] ? 'on' : 'off'}
+        </button>
+    )
+    return (
+      buttonList
+    )
+  }
+}
 
 function App() {
   return (
@@ -8,6 +32,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Hello world
+        <ButtonGroup buttons={[0, 1, 2]}></ButtonGroup>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
